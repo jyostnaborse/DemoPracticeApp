3389ce43eaeb13d288c7f6714ca34c425a9dfcfb
jyostnaborse <jyostnaborse@gmail.com>
2024-03-13 12:22:25 -0700

    Fixing typo

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -9,7 +9,7 @@ namespace API.Middleware
         private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IHostEnvironment _env;
-        er
+
         public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
             IHostEnvironment env)
         {
