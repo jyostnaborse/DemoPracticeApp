8a41acd395ba735c1b08f9b22c5fdf53547a2d6f
jyostnaborse <jyostnaborse@gmail.com>
2024-03-13 22:17:11 -0700

    Seeding Data in user table

--- API/Program.cs
+++ API/Program.cs
@@ -32,6 +32,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetService<ILogger>();
+    logger.LogError(ex, "An Error occurred during migartions");
+}
+
 app.Run();
 
 
