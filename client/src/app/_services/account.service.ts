132c0c7bb4814e657bf96d74c717eb4e64d32344
jyostnaborse <jyostnaborse@gmail.com>
2024-03-15 11:45:44 -0700

    Adding environment file for api url

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,13 +4,14 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { JsonPipe } from '@angular/common';
 import { user } from '../_models/user';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = "https://localhost:5001/api/"
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<user | null>(null);
   currentUser$= this.currentUserSource.asObservable();
 
